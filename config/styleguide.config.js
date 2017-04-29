6eb5f0ef191c9ffb0a454199ab8572b4e74cb2d1
Albert Fernández <albert@nandez.cat>
2017-04-29 20:38:16 +0200

    Upgrade dependencies

--- config/styleguide.config.js
+++ config/styleguide.config.js
@@ -2,53 +2,12 @@ const paths = require('./paths');
 const webpack = require('webpack');
 const postcssCssNext = require('postcss-cssnext');
 const postcssImport = require('postcss-import');
+const webpackConfig = require('./webpack.config.dev');
 
 module.exports = {
   title: 'Boilerplate Style Guide',
-  rootDir: paths.source,
-  components: './src/components/**/*.js',
+  components: '../src/components/**/*.js',
+  ignore: ['**/*.spec.js', '**/spec.js'],
   skipComponentsWithoutExample: true,
-
-  updateWebpackConfig: config => {
-    config.module.loaders.push(
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: paths.source
-      },
-      {
-        test: /\.css/,
-        loaders: [
-          'style-loader',
-          'css-loader?modules&localIdentName=[path][name]--[local]--[hash:base64:5]',
-          'postcss-loader'
-        ],
-        include: paths.source
-      },
-      {
-        test: /\.(woff|png|jpg|gif)$/,
-        include: paths.source,
-        loader: 'url-loader?limit=5000'
-      }
-    );
-
-    config.plugins.push(
-      new webpack.LoaderOptionsPlugin({
-        test: /\.css$/,
-        options: {
-          context: __dirname,
-          postcss: function(webpack) {
-            return [
-              postcssImport({
-                path: [paths.source, paths.nodeModules]
-              }),
-              postcssCssNext()
-            ];
-          }
-        }
-      })
-    );
-
-    return config;
-  }
+  webpackConfig
 };
