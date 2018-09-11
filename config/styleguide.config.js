e067a1534d9e6804c4d1e1320a95dca7c6a13fc1
Albert Fernández <albert@nandez.cat>
2018-09-12 00:31:14 +0200

    Remove react-styleguidist

--- config/styleguide.config.js
+++
@@ -1,9 +0,0 @@
-const webpackConfig = require('./webpack.config.dev')
-
-module.exports = {
-  title: 'Boilerplate Style Guide',
-  components: '../src/components/**/*.js',
-  ignore: ['**/*.spec.js', '**/spec.js'],
-  skipComponentsWithoutExample: true,
-  webpackConfig
-}
